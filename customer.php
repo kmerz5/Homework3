35a1ad3869eb0445eb16f614f32b8088f0479d11
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-21 14:33:34 -0500

    Update customer.php

--- customer.php
+++ customer.php
@@ -30,7 +30,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Customer_ID, Customer_FirstName, Customer_LastName, Restaurant_ID FROM Customer";
+$sql = "SELECT DISTINCT Customer_ID, Customer_FirstName, Customer_LastName, Restaurant_ID FROM Customer";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
