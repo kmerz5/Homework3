c8c274ad6287aea598ed2024dd827cccda41b006
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-21 14:29:52 -0500

    Update restaurant-employee.php

--- restaurant-employee.php
+++ restaurant-employee.php
@@ -1,1 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th>Restaurant Name</th>
+      <th>Employee First Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+$servername = "localhost";
+$username = "kyliemer_Homework3";
+$password = "ChunkyMonkey01!";
+$dbname = "kyliemer_Homework3";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT RestaurantName, Employee_FirstName FROM Restaurant r JOIN Employee e ON r.Restaurant_ID=e.Restaurant_ID";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["RestaurantName"]?></td>
+    <td><?=$row["Employee_FirstName"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+    </table>
