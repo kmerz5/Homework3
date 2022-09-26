dcd5e4de0662626516670100bc5a0d7899b7fcd7
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-26 14:21:30 -0500

    Update Employee-Restaurant.php

--- Employee-Restaurant.php
+++ Employee-Restaurant.php
@@ -54,7 +54,7 @@ if ($result->num_rows > 0) {
     $section_sql = "select e.Employee_FirstName from Employee e join Restaurant r on e.Restaurant_ID = r.Restaurant_ID where e.Restaurant_ID=" . $row["Restaurant_ID"];
     $section_result = $conn->query($section_sql);
      while($section_row = $section_result->fetch_assoc()) {
-      echo "<li>" . $section_row["description"] . "</li>";
+      echo "<li>" . $section_row["Employee_FirstName"] . "</li>";
     }
 ?>
       </ul></p>
