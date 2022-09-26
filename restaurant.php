5f425ba090eb4d88f1c0b88877f3ff9717f41cd2
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-26 13:38:15 -0500

    Update restaurant.php

--- restaurant.php
+++ restaurant.php
@@ -7,6 +7,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="customer.php">Customer</a>
+      <a class="nav-item nav-link" href="employee.php">Employee</a>
+      <a class="nav-item nav-link" href="purchase.php">Purchase</a>
+      <a class="nav-item nav-link" href="restaurant.php">Restaurant</a>
+      <a class="nav-item nav-link" href="restaurant-employee.php">Restaurant&Employee</a>
+      
+    </div>
+  </div>
+</nav>
+    
 <table class="table table-striped">
   <thead>
     <tr>
