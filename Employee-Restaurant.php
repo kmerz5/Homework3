5df8c250e48404a9a9538e6c01d4cd742f57e293
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-09-26 14:09:16 -0500

    Update Employee-Restaurant.php

--- Employee-Restaurant.php
+++ Employee-Restaurant.php
@@ -39,7 +39,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT DISTINCT RestaurantName, Employee_FirstName FROM Restaurant";
+$sql = "SELECT DISTINCT Restaurant_ID, Employee_FirstName FROM Employee";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
