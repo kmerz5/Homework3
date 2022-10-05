4ee5dd84574ad3c7a0b891780300c61feab3ee55
Kylie Merz <78387839+kmerz5@users.noreply.github.com>
2022-10-05 13:57:41 -0500

    Create customer-add-save.php

---
+++ customer-add-save.php
@@ -0,0 +1,12 @@
+
+
+<!doctype html>
+<html lang="en">
+  <Head>
+    <meta charset-"utf-8">
+    <meta name="viewport" content=width=device-width, initial-scale=1">
+   <h1> Add Customer</h1>
+<div class="alert alert-success" role="alert">
+  New Customer Added
+</div>
+    
